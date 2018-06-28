69aa13e13bd60797e9946dc83e98794e60ac4dc1
Patrick Tescher <pat2man@gmail.com>
2018-06-27 17:56:00 -0700

    Initial Import

---
+++ Assets/BitskiBehavior.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
+using System.Collections;
+using AOT;
+
+public class BitskiBehavior : MonoBehaviour
+{
+    private static BitskiBehavior callbackInstance;
+
+    public Button signInButton;
+
+    public SpriteRenderer[] characterSpriteRenderers;
+    private Sprite[] characterSprites;
+
+    void Start()
+    {
+        characterSprites = Resources.LoadAll<Sprite>("Characters");
+        signInButton.GetComponent<Button>().onClick.AddListener(SignInButtonClicked);
+    }
+
+    void SignInButtonClicked()
+    {
+        SignIn();
+    }
+
+    public IEnumerator GetAccountBalance(string accessToken) {
+        var nft = new NFT(accessToken);
+        yield return nft.GetAccountBalance();
+        showTokens(nft);
+        signInButton.enabled = false;
+    }
+
+    void showTokens(NFT nft) {
+        for (int i = 0; i < characterSpriteRenderers.Length; i++)
+        {
+            if (i >= nft.tokens.tokenIDs.Count - 1)
+            {
+                characterSpriteRenderers[i].enabled = false;
+                break;
+            }
+
+            var tokenID = nft.tokens.tokenIDs[i];
+            int spriteID = (int)(tokenID % 5);
+            characterSpriteRenderers[i].sprite = characterSprites[spriteID];
+            characterSpriteRenderers[i].enabled = true;
+        }
+
+    }
+
+    private delegate void BitskiSignInCallback(string token, string error);
+
+    #if UNITY_IPHONE
+    [DllImport("__Internal")]
+    #endif
+    private static extern void BitskiSignIn(string ClientID, string CallbackUri, BitskiSignInCallback callback);
+
+    void SignIn()
+    {
+        callbackInstance = this;
+        BitskiSignIn("35a7e890-2f64-4332-b5bc-ee556bde5cf1", "bitskiexampledapp://application/callback", SignInCallback);
+    }
+
+
+    [MonoPInvokeCallback(typeof(BitskiSignInCallback))]
+    static void SignInCallback(string accessToken, string error)
+    {
+        if (accessToken != null)
+        {
+            callbackInstance.didSignIn(accessToken);
+        }
+
+        if (error != null)
+        {
+            Debug.Log("Got an error" + error);
+        }
+
+        callbackInstance = null;
+    }
+
+    public void didSignIn(string accessToken)
+    {
+        StartCoroutine(GetAccountBalance(accessToken));
+    }
+}
---
+++ Assets/NFT.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using Nethereum.ABI.FunctionEncoding.Attributes;
+using Nethereum.Contracts;
+
+using System.Numerics;
+
+using UnityEngine;
+using Bitski.Rpc;
+
+public class NFT
+{
+    public static string ABI = @"[{'constant':true,'inputs':[{'name':'_owner','type':'address'}],'name':'getOwnerTokens','outputs':[{'name':'_tokenIds','type':'uint256[]'}],'payable':false,'stateMutability':'view','type':'function'}]";
+
+    private static string contractAddress = "0x8c51dff8fcd48c292354ee751cceabeb25357df4";
+    private Contract contract;
+    private string accessToken;
+    private string[] accounts;
+    public TokensDTO tokens;
+
+    public NFT(string accessToken)
+    {
+        this.accessToken = accessToken;
+        this.contract = new Contract(null, ABI, contractAddress);
+    }
+
+    public IEnumerator GetAccountBalance()
+    {
+        yield return GetAccounts();
+        yield return GetBalance(accounts[0]);
+    }
+
+    public IEnumerator GetAccounts()
+    {
+        var getDataCallUnityRequest = new EthAccountsBitskiUnityRequest(accessToken);
+        yield return getDataCallUnityRequest.SendRequest();
+        accounts = getDataCallUnityRequest.Result;
+    }
+
+    public IEnumerator GetBalance(string account)
+    {
+        var getDataCallUnityRequest = new EthCallBitskiUnityRequest(accessToken);
+        var function = contract.GetFunction("getOwnerTokens");
+        var callInput = function.CreateCallInput(account);
+        callInput.From = account;
+        callInput.To = contract.Address;
+
+        Debug.Log("callInput data is " + callInput.Data);
+        Debug.Log("callInput from is " + callInput.From);
+        Debug.Log("callInput to is " + callInput.To);
+
+        yield return getDataCallUnityRequest.SendRequest(callInput);
+        var result = getDataCallUnityRequest.Result;
+
+        tokens = new TokensDTO();
+        function.DecodeDTOTypeOutput<TokensDTO>(tokens, result);
+    }
+
+    [FunctionOutput]
+    public class TokensDTO
+    {
+        [Parameter("uint256[]", "_tokenIds", 1)]
+        public List<BigInteger> tokenIDs { get; set; }
+    }
+
+}
